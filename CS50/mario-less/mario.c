4a58ad4bde28ee6348f6bfb85335904d1fb3ff02
Nelson Cruz <nelson.443@icloud.com>
2025-02-24 16:14:28 -0500

    .

--- CS50/mario-less/mario.c
+++ CS50/mario-less/mario.c
@@ -8,14 +8,24 @@
 #include <stdio.h>
 #include "cs50.h"
 
+void print_row(int n);
+
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
+    const int n = 3;
+    
+    // Print n rows
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
+        print_row(n);
     }
+}
+
+void print_row(int n)
+{
+    for (int i =0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }

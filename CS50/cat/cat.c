df0021b70e705c8381fb6f0548e92dfc39b6f079
Nelson Cruz <nelson.443@icloud.com>
2025-02-24 14:30:56 -0500

    .

--- CS50/cat/cat.c
+++ CS50/cat/cat.c
@@ -8,17 +8,19 @@
 #include <stdio.h>
 #include "cs50.h"
 
-void meow(void);
+void meow(int n);
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
-    {
-        meow();
-    }
+    int n = get_int("Number: ");
+    meow(n);
 }
 
-void meow(void)
+void meow(int n)
 {
-    printf("meow\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+    
 }

326ea541dc19f517912dd7fdff78019d96b9fde2
Nelson Cruz <nelson.443@icloud.com>
2025-02-24 10:40:51 -0500

    .

---
+++ CS50/cat/cat.c
@@ -0,0 +1,16 @@
+//
+//  cat.c
+//  CS50
+//
+//  Created by Nelson Cruz on 2/24/25.
+//
+
+#include <stdio.h>
+#include "cs50.h"
+
+int main(void)
+{
+    printf("meow\n");
+    printf("meow\n");
+    printf("meow\n");
+}

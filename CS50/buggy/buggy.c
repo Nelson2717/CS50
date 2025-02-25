55288150cff69d4d6eb16cb0ffc040f99ff254e2
Nelson Cruz <nelson.443@icloud.com>
2025-02-25 15:55:47 -0500

    .

--- CS50/buggy/buggy.c
+++ CS50/buggy/buggy.c
@@ -6,10 +6,19 @@
 //
 
 #include <stdio.h>
+#include "cs50.h"
+
+void print_column(int height);
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
+    int h = get_int("Height: ");
+    print_column(h);
+}
+
+void print_column(int height)
+{
+    for (int i = 0; i < height; i++)
     {
         printf("#\n");
     }

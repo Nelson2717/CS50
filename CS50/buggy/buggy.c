71538ce78290ab85dcee290cbae5939f44fc5a54
Nelson Cruz <nelson.443@icloud.com>
2025-02-25 15:45:29 -0500

    .

---
+++ CS50/buggy/buggy.c
@@ -0,0 +1,17 @@
+//
+//  buggy.c
+//  CS50
+//
+//  Created by Nelson Cruz on 2/25/25.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i <= 3; i++)
+    {
+        printf("i is %i\n", i);
+        printf("#\n");
+    }
+}

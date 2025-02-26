80056882f372a9921b22c7f2d02dcc9958710835
Nelson Cruz <nelson.443@icloud.com>
2025-02-26 10:33:53 -0500

    .

--- CS50/hi/hi.c
+++ CS50/hi/hi.c
@@ -5,6 +5,7 @@
 //  Created by Nelson Cruz on 2/26/25.
 //
 
+/*
 #include <stdio.h>
 #include "cs50.h"
 
@@ -17,3 +18,4 @@ int main(void)
     printf("%c%c%c\n", words[0][0], words[0][1], words[0][2]);
     printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
 }
+*/

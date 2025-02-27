8d113c15956736248c9276d0d1ac89d4088db13e
Nelson Cruz <nelson.443@icloud.com>
2025-02-27 10:31:10 -0500

    .

---
+++ CS50/search/search.c
@@ -0,0 +1,27 @@
+//
+//  search.c
+//  CS50
+//
+//  Created by Nelson Cruz on 2/27/25.
+//
+
+#include <stdio.h>
+#include "cs50.h"
+
+int main(void)
+{
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+    
+    int n = get_int("Number: ");
+    for (int i = 0; i < 7; i++)
+    {
+        if (numbers[i] == n)
+        {
+            printf("Found!\n");
+        }
+        else
+        {
+            printf("Not found\n");
+        }
+    }
+}

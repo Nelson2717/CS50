d32827569b88e27385b56130af54834aafbae934
Nelson Cruz <nelson.443@icloud.com>
2025-02-27 09:16:24 -0500

    .

---
+++ CS50/status/status.c
@@ -0,0 +1,20 @@
+//
+//  status.c
+//  CS50
+//
+//  Created by Nelson Cruz on 2/27/25.
+//
+
+#include "cs50.h"
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Missing command-line argument\n");
+        return 1;
+    }
+    printf("hello, %s\n", argv[1]);
+    return 0;
+}

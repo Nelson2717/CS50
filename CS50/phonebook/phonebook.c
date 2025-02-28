e23c422dfddee521797f321a4344a88007529aff
Nelson Cruz <nelson.443@icloud.com>
2025-02-28 09:18:54 -0500

    .

--- CS50/phonebook/phonebook.c
+++ CS50/phonebook/phonebook.c
@@ -9,17 +9,31 @@
 #include "cs50.h"
 #include "string.h"
 
+typedef struct
+{
+    string name;
+    string number;
+} person;
+
 int main(void)
 {
-    string names[] = {"David", "John", "Yulia"};
-    string numbers [] = {"+1-617-495-100", "+1-949-468-2750", "+1-617-495-100"};
+    person people[3];
+    
+    people[0].name = "David";
+    people[0].number = "+1-617-495-100";
+    
+    people[1].name = "John";
+    people[1].number = "+1-949-468-2750";
+    
+    people[2].name = "Yulia";
+    people[2].number = "+1-617-495-100";
     
     string name = get_string("Name: ");
     for (int i = 0; i < 3; i++)
     {
-        if (strcmp(names[i], name) == 0)
+        if (strcmp(people[i].name, name) == 0)
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }

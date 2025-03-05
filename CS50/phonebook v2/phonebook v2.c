c827f6a2dabe564bc8a5374006346b8d8663b72c
Nelson Cruz <nelson.443@icloud.com>
2025-03-05 13:23:14 -0500

    .

--- CS50/phonebook v2/phonebook v2.c
+++ CS50/phonebook v2/phonebook v2.c
@@ -12,6 +12,10 @@
 int main(void)
 {
     FILE *file = fopen("phonebook.csv", "a");
+    if (file == NULL)
+    {
+        return 1;
+    }
     
     char *name = get_string("Name: ");
     char *number = get_string("Number: ");

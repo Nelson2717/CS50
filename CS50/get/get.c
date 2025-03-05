35496bdc2b45bf18d3791bb15956a6ee64f84d85
Nelson Cruz <nelson.443@icloud.com>
2025-03-05 11:39:08 -0500

    .

--- CS50/get/get.c
+++ CS50/get/get.c
@@ -9,8 +9,8 @@
 
 int main(void)
 {
-    int n;
-    printf("n: ");
-    scanf("%i", &n);
-    printf("n: %i\n", n);
+    char *s;
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
 }

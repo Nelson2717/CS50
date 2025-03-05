00806260c85d0dbcb64306d298ac5c9398bb515e
Nelson Cruz <nelson.443@icloud.com>
2025-03-05 15:07:41 -0500

    .

--- CS50/copy v2/copy v2.c
+++ CS50/copy v2/copy v2.c
@@ -6,42 +6,22 @@
 //
 
 #include <stdio.h>
-#include "stdint.h"
+#include <stdint.h>
 
 typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        printf("Usage: ./copy source destination\n");
-        return 1;
-    }
-    
-    FILE *src = fopen(argv[1], "r");
-    if (!src) // Check if file opened successfully.
-    {
-        printf("Error: cannot open %s\n", argv[1]);
-        return 1;
-    }
-    
-    FILE *dst = fopen(argv[2], "w");
-    if (!dst)
-    {
-        printf("Error: Cannot open %s\n", argv[2]);
-        fclose(src);
-        return 1;
-    }
-    
+    FILE *src = fopen(argv[1], "rb");
+    FILE *dst = fopen(argv[2], "wb");
+
     BYTE b;
-    
-    while (fread(&b, sizeof(b), 1, src))
+
+    while (fread(&b, sizeof(b), 1, src) != 0)
     {
         fwrite(&b, sizeof(b), 1, dst);
     }
-    
+
     fclose(dst);
     fclose(src);
-    
-    return 0;
 }

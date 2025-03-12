613650727c661e25ac28d3c1cb504016e4a385e6
Nelson Cruz <nelson.443@icloud.com>
2025-03-12 09:46:41 -0400

    .

--- CS50/list/list.c
+++ CS50/list/list.c
@@ -22,16 +22,11 @@ int main(void)
     
     // Time passes
     
-    int *tmp = malloc(4 * sizeof(int));
+    int *tmp = realloc(list, 4 * sizeof(int));
     if (tmp == NULL)
     {
         free(list);
         return 1;
-    }
-    
-    for (int i = 0; i < 3; i++)
-    {
-        tmp[i] = list[i];
     }
     
     tmp[3] = 4;

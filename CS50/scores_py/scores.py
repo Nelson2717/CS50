c4f8961818e630ce1b8c43b6efb52515a647a685
Nelson Cruz <nelson.443@icloud.com>
2025-03-18 11:47:50 -0400

    .

--- CS50/scores_py/scores.py
+++ CS50/scores_py/scores.py
@@ -1,6 +1,9 @@
-scores = [72, 73, 33]
+scores = []
+
+for i in range(3):
+    score = int(input("Score:"))
+    scores.append(score)
 
 average = sum(scores) / len(scores)
 
 print(f"Average: {average}")
-

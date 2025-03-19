fba9ff86fa2e6cdd9a5de7b29cb63b89a7071a3e
Nelson Cruz <nelson.443@icloud.com>
2025-03-19 09:51:22 -0400

    .

--- CS50/favorites_py/favorites.py
+++ CS50/favorites_py/favorites.py
@@ -4,4 +4,5 @@ with open("favorites.csv", "r") as file:
     reader = csv.reader(file)
     next(reader)
     for row in reader:
-        print(row[1])
+        favorite = row[1]
+        print(favorite)

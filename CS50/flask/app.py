6ee44460de28200cb9cd0673bd71d925ff9d5bc2
Nelson Cruz <nelson.443@icloud.com>
2025-04-02 11:58:00 -0400

    .

--- CS50/flask/app.py
+++ CS50/flask/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return ''
+    html = render_template("index.html")
+    return html

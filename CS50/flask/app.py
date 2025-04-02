c8c74b367481fccd8b686224065e59cbd49de080
Nelson Cruz <nelson.443@icloud.com>
2025-04-02 10:42:18 -0400

    .

--- CS50/flask/app.py
+++ CS50/flask/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<!DOCTYPE html><html lang="en"><head><title>hello, title</title></head><body>hello, body</body></html>'
+    return ''

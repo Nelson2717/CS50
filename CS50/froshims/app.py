215380724ecc94f37e2f3ab4cfa344a2e986092c
Nelson Cruz <nelson.443@icloud.com>
2025-04-08 12:39:21 -0400

    .

--- CS50/froshims/app.py
+++ CS50/froshims/app.py
@@ -2,3 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    return f"Hello, {name}. You selected {sport}!"

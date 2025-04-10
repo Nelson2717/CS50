8671e13ba0732dcd5eb03759d38d6f89eb258962
Nelson Cruz <nelson.443@icloud.com>
2025-04-10 14:12:20 -0400

    .

--- CS50/world/hello.c
+++ CS50/world/hello.c
@@ -1,15 +1,7 @@
-//
-//  hello.c
-//  CS50
-//
-//  Created by Nelson Cruz on 2/21/25.
-//
-
-/*
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello, world\n");
+    printf("hello, world!\n");
 }
-*/
+

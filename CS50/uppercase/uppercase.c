868cc0b9a43b256ad3e166ba39e33bcfb58e32d9
Nelson Cruz <nelson.443@icloud.com>
2025-04-10 14:23:32 -0400

    .

--- CS50/uppercase/uppercase.c
+++ CS50/uppercase/uppercase.c
@@ -1,24 +1,20 @@
-//
-//  uppercase.c
-//  CS50
-//
-//  Created by Nelson Cruz on 2/26/25.
-//
-
-/*
 #include <stdio.h>
-#include "cs50.h"
-#include "string.h"
-#include "ctype.h"
+#include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
-    string s = get_string("Before: ");
-    printf("After  ");
+    char s[100];
+
+    printf("Before: ");
+    fgets(s, sizeof(s), stdin);
+
+    printf("After: ");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
         printf("%c", toupper(s[i]));
     }
     printf("\n");
+
+    return 0;
 }
-*/
